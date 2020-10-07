08cc0fa6763794cd7adda3216d6055ae32f03eb6
Srihita Ayinala <srihita30@gmail.com>
2020-10-07 23:47:43 +0530

    constants added

---
+++ src/constants/index.js
@@ -0,0 +1,141 @@
+export const FCM_TOKEN = 'FCM_TOKEN';
+
+export const UPDATE_CONTENT = {
+  TITLE: 'You are using an old version',
+  CONTENT_1:
+    'We have released a new version of the app with lots of exciting new features',
+  CONTENT_2: '\nPlease update to the latest version',
+};
+export const APP_LINK = {
+  ios: 'itms-apps://itunes.apple.com/us/app/id1501299080?mt=8',
+  android: 'market://details?id=com.techmojo.app.sugarcare',
+};
+
+export const MY_HEALTH_TILES = {
+  PERSONAL_DETAILS: {
+    title: 'Personal Details',
+    subtitle: 'Your personal information',
+  },
+  HEALTH_ANALYSIS: {
+    title: 'Body Mass Index (BMI)',
+    subtitle: 'All about your health',
+  },
+  SLEEP_ANALYSIS: {
+    title: 'Sleep Analysis',
+    subtitle: 'Insights about your sleeping habits',
+  },
+  DISTANCE_ANALYSIS: {
+    title: 'Distance Walked',
+    subtitle: 'Your daily achievements',
+  },
+  PERSONAL_SUGGESTIONS: {
+    title: 'Health Tips',
+    subtitle: "Let's stay healthy",
+  },
+};
+
+export const USER_TITLES = [
+  {
+    value: 'Mr',
+  },
+  {
+    value: 'Ms',
+  },
+];
+
+export const USER_HEIGHT_FEET = [
+  {
+    value: '1 ft',
+  },
+  {
+    value: '2 ft',
+  },
+  {
+    value: '3 ft',
+  },
+  {
+    value: '4 ft',
+  },
+  {
+    value: '5 ft',
+  },
+  {
+    value: '6 ft',
+  },
+  {
+    value: '7 ft',
+  },
+  {
+    value: '8 ft',
+  },
+  {
+    value: '9 ft',
+  },
+];
+
+export const USER_HEIGHT_INCH = [
+  {
+    value: '0 in',
+  },
+  {
+    value: '1 in',
+  },
+  {
+    value: '2 in',
+  },
+  {
+    value: '3 in',
+  },
+  {
+    value: '4 in',
+  },
+  {
+    value: '5 in',
+  },
+  {
+    value: '6 in',
+  },
+  {
+    value: '7 in',
+  },
+  {
+    value: '8 in',
+  },
+  {
+    value: '9 in',
+  },
+  {
+    value: '10 in',
+  },
+  {
+    value: '11 in',
+  },
+];
+
+export const USER_DETAIL_KEYS = {
+  TITLE: 'title',
+  NAME: 'name',
+  MOBILE: 'mobile',
+  HEIGHT_FT: 'height_ft',
+  HEIGHT_IN: 'height_in',
+  WEIGHT: 'weight',
+};
+
+export const USER_BMI_STATE = {
+  OBESE: {
+    value: 'Obese',
+    color: '#D84551',
+  },
+  OVERWEIGHT: {
+    value: 'Overweight',
+    color: '#FFB346',
+  },
+  NORMAL: {
+    value: 'Normal',
+    color: '#279649',
+  },
+  UNDERWEIGHT: {
+    value: 'Underweight',
+    color: '#29ABF0',
+  },
+};
---
+++ src/constants/routes.js
@@ -0,0 +1,7 @@
+export default {
+  INITIATE_APP: 'INITIATE_APP',
+
+  APP_STACK: 'APP_STACK',
+  DASHBOARD: 'Dashboard',
+  MY_HEALTH: 'My Health',
+};
