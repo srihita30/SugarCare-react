41a5ef53ec0663a313e687e19058cdc0ac915c8d
Srihita Ayinala <srihita30@gmail.com>
2020-10-19 10:34:01 +0530

    theme file added

---
+++ src/styles/theme.js
@@ -0,0 +1,73 @@
+// COLORS
+export const color_alpha = '#8559EC';
+export const color_lightBlueGrey = '#C6D0DD';
+export const color_beta = '#E801EE';
+export const color_white = '#ffffff';
+export const color_black = '#000000';
+
+export const color_state_info = '#29ABF0';
+export const color_state_info_l = '#29ABF0';
+export const color_state_success = '#279649';
+export const color_state_success_l = '#bde0dd';
+export const color_state_warning = '#FFB346';
+export const color_state_warning_l = '#FFECB3';
+export const color_state_danger = '#D84551';
+export const color_state_danger_l = '#f1d6d6';
+
+export const color_bg = '#ECF0F5';
+export const color_bg_card = '#FFFFFF';
+export const color_bg_warning = color_state_warning;
+export const color_bg_success = color_state_success;
+export const color_bg_info = color_state_info;
+export const color_bg_danger = color_state_danger;
+
+export const color_txt = '#2A2D3E';
+export const color_txt_light = '#7A7D7E';
+export const color_txt_pale = 'rgba(255,255,255,0.8)';
+export const color_txt_inverse_light = '#C3C7F5';
+export const color_txt_inverse = '#FFFFFF';
+export const color_border = '#EAEAEA';
+
+// FONT FAMILIES
+export const font_family_alpha_t = 'Lato-Thin';
+export const font_family_alpha_l = 'Lato-Light';
+export const font_family_alpha_r = 'Lato-Regular';
+export const font_family_alpha_b = 'Lato-Bold';
+export const font_family_alpha_bl = 'Lato-Black';
+export const font_family_beta_r = 'NotoSans-Regular';
+export const font_family_beta_b = 'NotoSans-Bold';
+
+// FONT SIZES
+export const font_size_tiny = 10;
+export const font_size_smaller = 12;
+export const font_size_small = 14;
+export const font_size_base = 16;
+export const font_size_medium = 18;
+export const font_size_big = 20;
+export const font_size_bigger = 22;
+export const font_size_huge = 24;
+
+// ICON FONT SIZES
+export const icon_font_size_base = 15;
+export const icon_font_size_medium = 20;
+export const icon_font_size_big = 25;
+export const icon_font_size_bigger = 30;
+export const icon_font_size_huge = 35;
+
+// BORDER RADIUS
+export const border_radius_rounded_small = 4;
+export const border_radius_rounded = 8;
+export const border_radius_rounded_big = 15;
+export const border_radius_rounded_huge = 24;
+export const border_radius_circle = 50;
+
+// LETTER SPACING
+export const ltr_spc_small = 0.3;
+
+// GUTTER
+export const gutter_width_base = 15;
+export const gutter_width_tiny = 5;
+export const gutter_width_small = 10;
+export const gutter_width_medium = 20;
+export const gutter_width_big = 25;
+export const gutter_width_huge = 30;
